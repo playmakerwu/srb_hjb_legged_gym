7b348f0dee5f2679b1025576a5709f7dd22e4bc4
hwang-warren <hwang2446@wisc.edu>
2024-10-05 14:34:17 -0500

    added go1_flat task with flat gnd & no terrain measurement

--- legged_gym/envs/go1/flat/go1_flat_config.py
+++ legged_gym/envs/go1/flat/go1_flat_config.py
@@ -28,28 +28,47 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
-from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .base.legged_robot import LeggedRobot
-from .anymal_c.anymal import Anymal
-from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
-from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPPO
-from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
-from .cassie.cassie import Cassie
-from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
-from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .go1.rough.go1_rough_config import Go1RoughCfg, Go1RoughCfgPPO
-from .go1.flat.go1_flat_config import Go1FlatCfg, Go1FlatCfgPPO
+from legged_gym.envs.go1.rough.go1_rough_config import Go1RoughCfg, Go1RoughCfgPPO
 
+class Go1FlatCfg( Go1RoughCfg ):
+    class env( Go1RoughCfg.env ):
+        num_observations = 48
+  
+    class terrain( Go1RoughCfg.terrain ):
+        mesh_type = 'plane'
+        measure_heights = False
+  
+    class asset( Go1RoughCfg.asset ):
+        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
 
-import os
+    # class rewards( Go1RoughCfg.rewards ):
+    #     max_contact_force = 350.
+    #     class scales ( Go1RoughCfg.rewards.scales ):
+    #         orientation = -5.0
+    #         torques = -0.000025
+    #         feet_air_time = 2.
+    #         # feet_contact_forces = -0.01
+    
+    # class commands( Go1RoughCfg.commands ):
+    #     heading_command = False
+    #     resampling_time = 4.
+    #     class ranges( Go1RoughCfg.commands.ranges ):
+    #         ang_vel_yaw = [-1.5, 1.5]
 
-from legged_gym.utils.task_registry import task_registry
+    # class domain_rand( Go1RoughCfg.domain_rand ):
+    #     friction_range = [0., 1.5] # on ground planes the friction combination mode is averaging, i.e total friction = (foot_friction + 1.)/2.
 
-task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
-task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
-task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
-task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
-task_registry.register( "go1_rough", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO() )
-task_registry.register( "go1_flat", LeggedRobot, Go1FlatCfg(), Go1FlatCfgPPO())
-task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+class Go1FlatCfgPPO( Go1RoughCfgPPO ):
+    class policy( Go1RoughCfgPPO.policy ):
+        actor_hidden_dims = [128, 64, 32]
+        critic_hidden_dims = [128, 64, 32]
+        # activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+
+    # class algorithm( Go1RoughCfgPPO.algorithm):
+    #     entropy_coef = 0.01
+
+    class runner ( Go1RoughCfgPPO.runner):
+        run_name = ''
+        experiment_name = 'go1_flat'
+        # load_run = -1
+        # max_iterations = 300
