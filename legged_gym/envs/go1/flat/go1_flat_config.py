baed2bb35dd81f2f6a7fdde25a9e3d112a6d0a42
hwang-warren <hwang2446@wisc.edu>
2024-10-20 12:05:39 -0500

    <go1_flat>: add base height & orientation rwd, rmv feet_air_time, looks good but foot is slslipping on gnd

--- legged_gym/envs/go1/flat/go1_flat_config.py
+++ legged_gym/envs/go1/flat/go1_flat_config.py
@@ -28,46 +28,144 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-from legged_gym.envs.go1.rough.go1_rough_config import Go1RoughCfg, Go1RoughCfgPPO
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
-class Go1FlatCfg( Go1RoughCfg ):
-    class env( Go1RoughCfg.env ):
-        num_observations = 48
+class Go1FlatCfg( LeggedRobotCfg ):
+    class env( LeggedRobotCfg.env ):
+        num_observations = 49
   
-    class terrain( Go1RoughCfg.terrain ):
-        mesh_type = 'plane'
-        measure_heights = False
-  
-    class asset( Go1RoughCfg.asset ):
-        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
-
-    # class rewards( Go1RoughCfg.rewards ):
-    #     max_contact_force = 350.
-    #     class scales ( Go1RoughCfg.rewards.scales ):
-    #         orientation = -5.0
-    #         torques = -0.000025
-    #         feet_air_time = 2.
-    #         # feet_contact_forces = -0.01
+    class terrain( LeggedRobotCfg.terrain ):
+        mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
+        measure_heights = True
+        measured_points_x = [0.] # measure height at base to get base rel height
+        measured_points_y = [0.]
     
-    # class commands( Go1RoughCfg.commands ):
-    #     heading_command = False
-    #     resampling_time = 4.
-    #     class ranges( Go1RoughCfg.commands.ranges ):
-    #         ang_vel_yaw = [-1.5, 1.5]
+    class commands( LeggedRobotCfg.commands ):
+        curriculum = False
+        max_curriculum = 1.
+        num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 4. # time before command are changed[s]
+        heading_command = False # if true: compute ang vel command from heading error
+        class ranges( LeggedRobotCfg.commands.ranges ):
+            lin_vel_x = [-1.0, 1.0] # min max [m/s]
+            lin_vel_y = [-1.0, 1.0]   # min max [m/s]
+            ang_vel_yaw = [-1.0, 1.0]    # min max [rad/s]
+            # # heading = [-3.14, 3.14]
+    
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.0,    # [rad]
+            'RL_hip_joint': 0.0,    # [rad]
+            'FR_hip_joint': 0.0,    # [rad]
+            'RR_hip_joint': 0.0,    # [rad]
 
-    # class domain_rand( Go1RoughCfg.domain_rand ):
-    #     friction_range = [0., 1.5] # on ground planes the friction combination mode is averaging, i.e total friction = (foot_friction + 1.)/2.
+            'FL_thigh_joint': 0.8,  # [rad]
+            'RL_thigh_joint': 0.8,  # [rad]
+            'FR_thigh_joint': 0.8,  # [rad]
+            'RR_thigh_joint': 0.8,  # [rad]
 
-class Go1FlatCfgPPO( Go1RoughCfgPPO ):
-    class policy( Go1RoughCfgPPO.policy ):
+            'FL_calf_joint': -1.6,  # [rad]
+            'RL_calf_joint': -1.6,  # [rad]
+            'FR_calf_joint': -1.6,  # [rad]
+            'RR_calf_joint': -1.6,  # [rad]
+        }
+    
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 20.}  # [N*m/rad]
+        damping = {'joint': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+  
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go1/urdf/go1.urdf'
+        name = "go1"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base"]
+        collapse_fixed_joints = True # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
+        default_dof_drive_mode = 3 # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        replace_cylinder_with_capsule = True # replace collision cylinders with capsules, leads to faster/more stable simulation
+
+    class domain_rand( LeggedRobotCfg.domain_rand ):
+        randomize_friction = True
+        friction_range = [0.5, 1.25]
+        randomize_base_mass = False
+        added_mass_range = [-1., 1.]
+        push_robots = True
+        push_interval_s = 15
+        max_push_vel_xy = 1.
+    
+    class rewards( LeggedRobotCfg.rewards ):
+        class scales( LeggedRobotCfg.rewards.scales ):
+            # termination = -0.0
+            tracking_lin_vel = 1.0
+            tracking_ang_vel = 0.5
+            lin_vel_z = -2.0
+            ang_vel_xy = -0.05
+            
+            orientation = -5.
+            base_height = -5.
+
+            torques = -0.00001
+            # dof_vel = -0.
+            dof_acc = -2.5e-7
+            action_rate = -0.01
+             
+            feet_air_time = 0.0
+            collision = -1.
+            dof_pos_limits = -10.0
+            # feet_stumble = -0.0 
+            # stand_still = -0.
+
+        only_positive_rewards = True # if true negative total rewards are clipped at zero (avoids early termination problems)
+        tracking_sigma = 0.25 # tracking reward = exp(-error^2/sigma)
+        soft_dof_pos_limit = 1. # percentage of urdf limits, values above this limit are penalized
+        soft_dof_vel_limit = 1.
+        soft_torque_limit = 1.
+        base_height_target = 0.34
+        max_contact_force = 100. # forces above this value are penalized
+    
+    class normalization( LeggedRobotCfg.normalization ):
+        class obs_scales( LeggedRobotCfg.normalization.obs_scales ):
+            lin_vel = 2.0
+            ang_vel = 0.25
+            dof_pos = 1.0
+            dof_vel = 0.05
+            height_measurements = 5.0
+        clip_observations = 100.
+        clip_actions = 100.
+
+    class noise( LeggedRobotCfg.noise ):
+        add_noise = True
+        noise_level = 1.0 # scales other values
+        class noise_scales( LeggedRobotCfg.noise.noise_scales ):
+            dof_pos = 0.01
+            dof_vel = 1.5
+            lin_vel = 0.1
+            ang_vel = 0.2
+            gravity = 0.05
+            height_measurements = 0.1
+    
+    class sim( LeggedRobotCfg.sim ):
+        dt =  0.005
+        substeps = 1
+        gravity = [0., 0. ,-9.81]  # [m/s^2]
+        up_axis = 1  # 0 is y, 1 is z
+
+
+class Go1FlatCfgPPO( LeggedRobotCfgPPO ):
+    class policy( LeggedRobotCfgPPO.policy ):
         actor_hidden_dims = [128, 64, 32]
         critic_hidden_dims = [128, 64, 32]
-        # activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+        activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
 
-    # class algorithm( Go1RoughCfgPPO.algorithm):
-    #     entropy_coef = 0.01
-
-    class runner ( Go1RoughCfgPPO.runner):
+    class runner ( LeggedRobotCfgPPO.runner):
         run_name = ''
         experiment_name = 'go1_flat'
         # load_run = -1
