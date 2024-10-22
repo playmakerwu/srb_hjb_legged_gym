1eb8a11b303c4e275d0a861e45652c25cf8f1416
hwang-warren <hwang2446@wisc.edu>
2024-10-22 17:46:27 -0500

    <go1_flat>: enable self-collision; <play.py>: keep push_robots cfg

--- legged_gym/envs/go1/flat/go1_flat_config.py
+++ legged_gym/envs/go1/flat/go1_flat_config.py
@@ -89,7 +89,7 @@ class Go1FlatCfg( LeggedRobotCfg ):
         terminate_after_contacts_on = ["base"]
         collapse_fixed_joints = True # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
         default_dof_drive_mode = 3 # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
-        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
         replace_cylinder_with_capsule = True # replace collision cylinders with capsules, leads to faster/more stable simulation
 
     class domain_rand( LeggedRobotCfg.domain_rand ):
