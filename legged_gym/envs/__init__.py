0f2834889b1a07dfb4061d79b89d3dc6f128a55c
hwang-warren <hwang2446@wisc.edu>
2025-01-01 17:44:01 -0600

    <go1_modle>: use ssh url for git submodule; <env/__init__.py>: deleted v config

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -40,7 +40,6 @@ from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .go1.rough.go1_rough_config import Go1RoughCfg, Go1RoughCfgPPO
 from .go1.flat.go1_flat_config import Go1FlatCfg, Go1FlatCfgPPO
-from .go1.flat.go1_flat_roller_v_config import Go1FlatRollerVCfg, Go1FlatRollerVCfgPPO
 from .go1.flat.go1_flat_roller_x_config import Go1FlatRollerXCfg, Go1FlatRollerXCfgPPO
 
 
@@ -54,6 +53,5 @@ task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPP
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "go1_rough", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO() )
 task_registry.register( "go1_flat", LeggedRobot, Go1FlatCfg(), Go1FlatCfgPPO())
-task_registry.register( "go1_flat_roller_v", LeggedRobot, Go1FlatRollerVCfg(), Go1FlatRollerVCfgPPO())
 task_registry.register( "go1_flat_roller_x", LeggedRobot, Go1FlatRollerXCfg(), Go1FlatRollerXCfgPPO())
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
